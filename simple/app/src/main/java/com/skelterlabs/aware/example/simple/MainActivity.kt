da599d8c4649cc9854c1868ad005ba4699b7fd79
hyungsun <hyungsun@wisefour.com>
2019-11-12 14:51:47 +0900

    Introduce request-permission sample codes for wanted signals
    Change-Id: I9972c4d12f6ab59a755055882f0a9dcc6c68c0ff

--- simple/app/src/main/java/com/skelterlabs/aware/example/simple/MainActivity.kt
+++ simple/app/src/main/java/com/skelterlabs/aware/example/simple/MainActivity.kt
@@ -1,49 +1,71 @@
 package com.skelterlabs.aware.example.simple
 
+import android.Manifest
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import com.skelterlabs.aware.AIQAware
 import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.sdk27.coroutines.onClick
 import org.jetbrains.anko.toast
 
 class MainActivity : AppCompatActivity() {
 
+  companion object {
+    private const val PERMISSION_REQUEST_CODE = 1234
+  }
+
   private lateinit var aiqAware: AIQAware
+  private val disposableBag = CompositeDisposable()
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+
     aiqAware = (application as SimpleApp).aiqAware
+
     register.onClick {
-      aiqAware.register()
+      val registration = aiqAware.register()
         .observeOn(AndroidSchedulers.mainThread())
         .doFinally { updateView() }
         .subscribe(
           { toast("AIQAware is registered") },
           { error -> toast("Error occur when registering: $error") }
         )
+
+      disposableBag.add(registration)
     }
     unregister.onClick {
-      aiqAware.unregister()
+      val unregistration = aiqAware.unregister()
         .observeOn(AndroidSchedulers.mainThread())
         .doFinally { updateView() }
         .subscribe(
           { toast("AIQAware is unregistered") },
           { error -> toast("Error occur when unregistering: $error") }
         )
+
+      disposableBag.add(unregistration)
     }
     toggle_service_enabled.onClick {
       aiqAware.serviceEnabled = !aiqAware.serviceEnabled
       updateView()
     }
+
+    requestPermissions()
     updateView()
   }
 
   override fun onResume() {
     super.onResume()
     updateView()
+  }
+
+  override fun onDestroy() {
+    super.onDestroy()
+
+    disposableBag.clear()
   }
 
   private fun updateView() {
@@ -61,5 +83,18 @@ class MainActivity : AppCompatActivity() {
     } else {
       is_service_enabled.text = getString(R.string.is_service_disabled)
     }
+  }
+
+  private fun requestPermissions() {
+
+    val permissions = listOf(
+      Manifest.permission.BLUETOOTH,
+      Manifest.permission.READ_CALENDAR,
+      Manifest.permission.ACCESS_FINE_LOCATION,
+      Manifest.permission.ACCESS_WIFI_STATE
+    )
+      .toTypedArray()
+
+    ActivityCompat.requestPermissions(this, permissions, PERMISSION_REQUEST_CODE)
   }
 }
